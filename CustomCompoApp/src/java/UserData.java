0ee06b18b4bc864d3f7be942de0198003f035d41
Mahesh Giri <mahesh@vibeosys.com>
2016-01-23 00:59:18 +0530

    Custom Componenet Created and used using jsf lib

---
+++ CustomCompoApp/src/java/UserData.java
@@ -0,0 +1,40 @@
+
+import javax.faces.bean.ManagedBean;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hungry
+ */
+@ManagedBean
+public class UserData {
+    String username;
+    String password;
+
+    public String login(){
+        return "result";
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+    
+}
