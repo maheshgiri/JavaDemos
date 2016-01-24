35b0a8c6e236bb476ca16b11713e08a549a2f171
mahesh.giri6486@gmail.com <>
2016-01-24 19:30:59 +0530

    Custom  Tags Build in Jsf using comonents

---
+++ CustomTag/src/java/User.java
@@ -0,0 +1,18 @@
+
+import javax.faces.bean.ManagedBean;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hungry
+ */
+@ManagedBean
+public class User {
+ public User(){
+     
+ }   
+}
